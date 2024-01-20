bc2675bed4e4f98e52dce0bb2c5c48658b96fdd4
Andrew <63685025+Andcr07@users.noreply.github.com>
2024-01-20 14:52:40 -0500

    FULL REFRESH
    stuff was screwed up

--- addons/qr_code/plugin.gd
+++ addons/qr_code/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	self.add_custom_type("QRCodeRect", "TextureRect", preload("qr_code_rect.gd"), preload("qr_code.svg"))
+    self.add_custom_type("QRCodeRect", "TextureRect", preload("qr_code_rect.gd"), preload("qr_code.svg"))
 
 func _exit_tree() -> void:
-	self.remove_custom_type("QRCodeRect")
+    self.remove_custom_type("QRCodeRect")
