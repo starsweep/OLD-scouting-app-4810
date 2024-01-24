770278393b2aab6e754bebb92ba57fc42d401115
Shortshortstop03 <shortshortstop03@gmail.com>
2024-01-23 20:32:22 -0500

    ytfghjko876tfghi98765r

--- scripts/QRnum.gd
+++ scripts/QRnum.gd
@@ -5,12 +5,17 @@ var QRnum
 var mnum
 var alliance
 var Aamp
+var Aamp_text
 var Aspeaker
+var Aspeaker_text
 var Aleft
 var Aleft_text
 var amp
+var amp_text
 var speaker
+var speaker_text
 var amplified
+var amplified_text
 var defense
 var chainrobots
 var spotlit
@@ -39,8 +44,9 @@ func _ready():
 		alliance.text = "002"
 	defense.text = "00" + defense.text
 	chainrobots.text = "00" + chainrobots.text
+	
 func _process(_delta):
-	QRnum.text = tnum.text + mnum.text + alliance.text + Aamp.text + Aspeaker.text
-	QRnum.text = QRnum.text + Aleft_text.text + amp.text + speaker.text + amplified.text
-	QRnum.text = QRnum.text + defense.text + chainrobots.text + spotlit_text.text
-	QRnum.text = QRnum.text + parked_text.text
+	QRnum.text = tnum.text + mnum.text + alliance.text# + Aamp_text.text
+	#QRnum.text = QRnum.text + Aspeaker_text.text + Aleft_text.text + amp_text.text
+	#QRnum.text = QRnum.text + speaker_text.text + amplified_text.text + defense.text
+	#QRnum.text = QRnum.text + chainrobots.text # + spotlit_text.text + parked_text.text
