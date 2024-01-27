bec4fdca9a1fa8028c1ea548767d92a02bcdf14c
Andrew <63685025+Andcr07@users.noreply.github.com>
2024-01-27 10:30:40 -0500

    fixed bad code to do what it was supposed to

--- scripts/Text_limit.gd
+++ scripts/Text_limit.gd
@@ -1,13 +1,14 @@
 extends Node
 
+var tnum
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
-func text_changed():
-	if $Tnum.get_text().length() > 5:
+	tnum = get_node("/root/UserInput/QR_Number/Tnum")
+	
+
+func _on_text_changed():
+	if tnum.text.length() >= 5:
 		#Disable Typing
-		$Tnum.readonly = true
+		tnum.editable = false
