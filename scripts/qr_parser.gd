4c41e6753576bd969cabf551a55e35033284694c
Shortshortstop03 <shortshortstop03@gmail.com>
2024-01-27 15:03:36 -0500

    fixed major fuck up

--- scripts/qr_parser.gd
+++ scripts/qr_parser.gd
@@ -4,16 +4,11 @@ var text = "THIS IS YOUR STRING"
 var arr = [ ]
 
 var offset = 3
-var teamlength = 5
 
 func called():
 	
 	var i = 0
 	
-	arr.push_back(text.substr(teamlength, 0))
-	
-	while teamlength + (i * 3) < text.length():
-		
-		arr.push_back(text.substr(teamlength + (i * offset), offset))
-		
+	while i * 5 < text.length():
+		arr.push_back( text.substr(i * offset, offset) )
 		i += 1
