de1127a5dd7352a75c319fab2ee3747ed4e20a95
Shortshortstop03 <shortshortstop03@gmail.com>
2024-01-26 20:31:03 -0500

    1345yhgfdswq

--- scripts/Text_limit.gd
+++ scripts/Text_limit.gd
@@ -1,15 +1,13 @@
 extends Node
 
-var tnum
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	tnum = get_node("/root/UserInput/QR_Number/Tnum")
 	
-func _on_TextEdit_text_changed():
-	if tnum.length() > 5:
+func text_changed():
+	if $Tnum.get_text().length() > 5:
 		#Disable Typing
-		tnum.text = tnum.left(tnum.length() - 1)
+		$Tnum.readonly = true
