d93a3c8f8580ece5fa4109e4b2a346383143882a
Andrew <63685025+Andcr07@users.noreply.github.com>
2024-01-29 19:35:12 -0500

    made export layout

--- scripts/csv exporter.gd
+++ scripts/csv exporter.gd
@@ -1,10 +1,43 @@
 extends Button
 
 var QRnum
+var arr = [ ]
+var TeamLength = 5
+var offset = 3
 
 func _ready():
 	
 	QRnum = get_node("/root/UserInput/QR_Number/QRnum")
 
 func _on_pressed():
-	pass # Replace with function body.
+	
+	var i = 0
+	
+	arr.push_back(str_to_var(QRnum.text.substr(0, TeamLength)))
+	
+	while (i * offset) + TeamLength < QRnum.text.length():
+		
+		arr.push_back(str_to_var(QRnum.text.substr((i * offset) + TeamLength, offset)))
+		i += 1
+	
+	#--------------------
+	# Array slot legend: 
+	# 0 - team number
+	# 1 - match number
+	# 2 - alliance (001 = red, 002 = blue)
+	# 3 - auton amp scores
+	# 4 - auton speaker scores
+	# 5 - tele-op amp scores
+	# 6 - tele-op speaker scores
+	# 7 - amped speaker scores
+	# 8 - defense score (subjective)
+	# 9 - bots on chain (number)
+	#10 - spotlit (001 = on, 000 = off)
+	#12 - parked (001 = yes, 000 = no)
+	#13 - Left starting area in auton (001 = yes, 000 = no)
+	#14 - Notes collected from source
+	#15 - notes collected from ground
+	#--------------------
+	
+	var file = FileAccess.open("user://export.csv", FileAccess.WRITE)
+	file.store_string("ass")
