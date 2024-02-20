7daabbd8a3cec252e7bdae8e5cb8666318bd25c8
Andrew <andrewmartinatm@gmail.com>
2024-02-19 19:05:30 -0500

    fixed some file extension errors

--- .godot/mono/temp/obj/Debug/scouting app 4810.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/scouting app 4810.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scouting app 4810")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0664a4748a1ad80ab8931ee93e4196220542f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("scouting app 4810")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scouting app 4810")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
